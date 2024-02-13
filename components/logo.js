11755813641a06b2c9e1e3c37d47876e0c1ec878
yifancodeWin <yifan.wang2@stud.uni-hannover.de>
2024-02-13 23:36:13 +0100

    Resolved some errors that occurred due to NextLink, logo and navbar work fine now

--- components/logo.js
+++ components/logo.js
@@ -1,21 +1,21 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import { Text, useColorModeValue } from '@chakra-ui/react';
+import Link from 'next/link'
+import Image from 'next/image'
+import { Text, useColorModeValue } from '@chakra-ui/react'
 import FootprintIcon from './icons/footprint'
-import styled from '@emotion/styled';
+import styled from '@emotion/styled'
 
 const LogoBox = styled.span`
-    font-weight: bold;
-    font-size: 18px;
-    display: inline-flex;
-    align-items: center;
-    height: 30px;
-    line-height: 20px;
-    padding: 10px;
+  font-weight: bold;
+  font-size: 18px;
+  display: inline-flex;
+  align-items: center;
+  height: 30px;
+  line-height: 20px;
+  padding: 10px;
 
-    &:hover img {
-        transform: rotate(20deg);
-    }
+  &:hover > svg {
+    transform: rotate(20deg);
+  }
 `
 
 // const Logo = () => {
@@ -46,17 +46,17 @@ const Logo = () => {
 
     return (
         <Link href="/">
-            <LogoBox>
-                <Image src={footPrintImg} width={20} height={20} alt="logo" />
-                <Text
-                    color={useColorModeValue('gray.800', 'whiteAlpha.900')}a
-                    fontFamily="M PLUS Rounded 1c"
-                    fontWeight="bold"
-                    ml={3}
-                >
-                    Yifan Wang
-                </Text>
-            </LogoBox>
+                <LogoBox>
+                    <Image src={footPrintImg} width={20} height={20} alt="logo" />
+                    <Text
+                        color={useColorModeValue('gray.800', 'whiteAlpha.900')}
+                        fontFamily='M PLUS Rounded 1c", sans-serif'
+                        fontWeight="bold"
+                        ml={3}
+                    >
+                        Yifan Wang
+                    </Text>
+                </LogoBox>
         </Link>
     )
 }
--- components/navbar.js
+++ components/navbar.js
@@ -14,22 +14,24 @@ import {
     IconButton,
     useColorModeValue
 } from '@chakra-ui/react'
-import { HumburgerIcon } from '@chakra-ui/icons'
+import { HamburgerIcon } from '@chakra-ui/icons'
 
-const LinkItem = ({ href, path, children}) => {
+const LinkItem = ({ href, path, target, children, ...props}) => {
     const active = path === href
-    const inactiveColor =useColorModeValue('gray.200', 'whilteAlpha.900')
+    const inactiveColor = useColorModeValue('gray.200', 'whilteAlpha.900')
     return(
-        <NextLink href={href}>
-            <Link
-                P={2}
-                bg={active ? 'glassTeal' : undefined}
-                color={active ? '#202023' : inactiveColor}>
-                {children}
-            </Link>
-        </NextLink>
+        <Link
+            as={NextLink}
+            href={href}
+            P={2}
+            bg={active ? 'grassTeal' : undefined}
+            color={active ? '#202023' : inactiveColor}
+            target={target}
+            {...props}
+        >
+            {children}
+        </Link>
     )
-
 }
 
 const Navbar = props => {
@@ -45,19 +47,27 @@ const Navbar = props => {
             zIndex={1}
             {...props}    
         >
-            <Container display="flex" p={2} maxW="container.md" warp="wrap" align="center" justify="space-between">
+            <Container 
+                display="flex"
+                p={2}
+                maxW="container.md"
+                wrap="wrap"
+                align="center"
+                justify="space-between"
+            >
                 <Flex align="center" mr={5}>
                     <Heading as="h1" size='lg' letterSpacing={'tighter'}>
                         <Logo />
                     </Heading>
                 </Flex>
+
                 <Stack 
-                    direction={{base: 'column', md: 'row'}}
-                    dispaly={{base: 'none', md: 'flex'}}
-                    width={{base: 'full', md: 'auto'}}
+                    direction={{ base: 'column', md: 'row' }}
+                    display={{ base: 'none', md: 'flex' }}
+                    width={{ base: 'full', md: 'auto' }}
                     alignItems="center"
                     flexGrow={1}
-                    mt={{base: 4, md: 0}}
+                    mt={{ base: 4, md: 0 }}
                 >
                     <LinkItem href="/works" path={path}>
                         Works                    
@@ -66,6 +76,27 @@ const Navbar = props => {
                         Posts  
                     </LinkItem>           
                 </Stack>
+
+                <Box flex={1} align="right">
+                    <Box ml={2} display={{base: 'inline-block', md: 'none'}}>
+                        <Menu>
+
+                            <MenuButton 
+                            as={IconButton} 
+                            icon={<HamburgerIcon />} 
+                            variant="outline" 
+                            aria-label="Options"
+                            />
+
+                            <MenuList>
+                                <MenuItem as={Link} href="/">About</MenuItem>
+                                <MenuItem as={Link} href="/">Works</MenuItem>
+                                <MenuItem as={Link} href="https://github.com/sIHURs/sihurs_homepage">Source</MenuItem>
+                            </MenuList>
+
+                        </Menu>
+                    </Box>
+                </Box>
             </Container>
         </Box>
     )
