042b49e6abea9f3262606c0c09f74997d33eab0e
yifancodeWin <yifan.wang2@stud.uni-hannover.de>
2024-02-13 19:08:30 +0100

    init create

---
+++ components/layouts/main.js
@@ -0,0 +1,52 @@
+import Head from "next/head"
+import dynamic from 'next/dynamic'
+// import NavBar from '../navbar'
+import { Box, Container } from '@chakra-ui/react'
+// import Footer from '../footer'
+// import VoxelDogLoader from '../vocel-dog-loader'
+
+// * 3D- model
+
+// const LazyVocelDog = dynamic(() => import('../voxel-dog'), {
+//     ssr: false,
+//     loading: () => <VoxelDogLoader />
+// })
+
+
+const Main = ({ children, router}) => {
+    return (
+        <Box as="main" pd={8}>
+            <Head>
+                <meta name="viewport" content="width-device-width, initial-scale=1" />
+                {/* <meta name="discription" content="Yifan's homapage" /> */}
+                {/* <meta name="author" content="Yifan Wang" />
+                <meta name="author" content="Sihurs"/>
+                <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+                <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+                <meta name="twitter:title" content="Yifan Wang" />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:site" content="@Sihurs" />
+                <meta name="twitter:creator" content="@Sihurs" /> */}
+                {/* <meta name="twitter:image" content="https://www.craftz.dog/card.png" /> */}
+                {/* <meta property="og:site_name" content="Yifan Wang" />
+                <meta name="og:title" content="Yifan Wang" />
+                <meta property="og:type" content="website" /> */}
+                {/* <meta property="og:image" content="https://www.craftz.dog/card.png" /> */}
+                <title>Yifan Wang - Homepage</title>
+            </Head>
+
+            {/* <NavBar path={router.asPath} /> */}
+
+            <Container maxW="container.md" pt={14}>
+                {/* <LazyVoxelDog /> */}
+
+                {children}
+
+                {/* <Footer /> */}
+            </Container>
+
+        </Box>
+    )
+} 
+
+export default Main
---
+++ pages/_app.js
@@ -0,0 +1,19 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import Layout from '../components/layouts/main'
+// import Fonts from '../components/fonts'
+// import { AnimatePresence } from 'framer-motion'
+// import Chakra from '../components/chakra'
+// import Phyhip from '../components/phyhip'
+// import { Analytics } from '@vercel/analytics/react'
+
+const Website = ( {Component, pageProps, router }) => {
+    return (
+        <ChakraProvider>
+            <Layout router={router}>
+                <Component {...pageProps} key={router.route} />
+            </Layout>
+        </ChakraProvider>
+    )
+}
+
+export default Website
