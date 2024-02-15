9abdedfd84f970e3e6d409316b7b7c8eab6053b9
yifancodeWin <yifan.wang2@stud.uni-hannover.de>
2024-02-15 14:39:59 +0100

    add profile image

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
-import { Container, Box, Heading } from "@chakra-ui/react"
+import { Container, Box, Heading, useColorModeValue } from "@chakra-ui/react"
+import Image from 'next/image'
 
 const Page = () => {
     return (
         <Container>
-            <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+            <Box 
+                borderRadius="lg" 
+                bg={useColorModeValue('whilteAlpha.500', 'whiteAlpha.200')} 
+                p={3} 
+                mb={6} 
+                align="center"
+            >
                 Hello, i&apos;m a ML Student from China!
             </Box>
 
@@ -13,6 +20,30 @@ const Page = () => {
                         Yifan Wang
                     </Heading>
                     <p>LUH Master Student, German - China </p>
+                </Box>
+                <Box 
+                    flexShrink={0}
+                    mt={{ base: 4, md: 0}}
+                    ml={{ md: 6 }}
+                    textAlign="center"
+                >
+                    <Box
+                        borderColor="whilteAlpha.800"
+                        bordwidth={2}
+                        borderStyle="solid"
+                        w="100px"
+                        h="100px"
+                        display="inline-block"
+                        borderRadius="full"
+                        overflow="hidden"
+                    >
+                        <Image
+                            src="/images/me.png"
+                            alt="profile image"
+                            width="100"
+                            height="100"
+                        />
+                    </Box>
                 </Box>
             </Box>
         </Container>
